01657f235ca2040b87baae48094c98ab768b4279
Kiryl Andruski <kiryl.andruski@gmail.com>
2022-03-06 13:22:00 +0100

    func

--- find_routes_match_draft.sql
+++ find_routes_match_draft.sql
@@ -1,81 +1,93 @@
-DROP TABLE trip_route;
-CREATE TABLE trip_route
-(
-    name                 VARCHAR,
-    max_deviation_meters REAL,
-    route                geometry(LineString, 4326)
-);
+CREATE OR REPLACE FUNCTION match_partial_trips(
+    trip_ids uuid[],
+    driver_route_polyline VARCHAR,
+    driver_max_deviation_meters REAL
+)
+    RETURNS TABLE
+            (
+                result_trip_id uuid
+            )
+AS
+$$
+DECLARE
+    driver_route      geography := st_linefromencodedpolyline(driver_route_polyline)::geography;
+    driver_route_geom geometry  = driver_route::geometry;
+BEGIN
+    RETURN QUERY
+        WITH
+            trips_routes                    AS (
+                SELECT id                             AS trip_id,
+                       route                          AS trip_route,
+                       route::geometry                AS trip_route_geom,
+                       st_startpoint(route::geometry) AS trip_route_start
+                FROM trips_trip
+                WHERE id = ANY (trip_ids)
+            ),
 
-INSERT INTO trip_route(name, max_deviation_meters, route)
-VALUES ('1', 5000,
-        ST_GeomFromGeoJSON('{"type":"LineString","coordinates":[[23.4942626953125,51.13455469147683],[22.56591796875,51.25847731518399],[21.851806640625,51.570241445811234],[21.000366210937496,52.2378923494213],[20.36865234375,53.12040528310657]]}')),
-       ('2', 5000,
-        ST_GeomFromGeoJSON('{"type":"LineString","coordinates":[[23.4832763671875,51.13455469147683],[22.8955078125,51.64188525876834],[22.3846435546875,51.940878612558684],[22.2857666015625,52.17056279155013],[21.5386962890625,52.18403686498285],[20.9783935546875,52.24125614966341],[19.9237060546875,52.1098789403549],[19.423828125,51.781435604431195]]}')),
-       ('3', 10000,
-        ST_GeomFromGeoJSON('{"type":"LineString","coordinates":[[22.730712890625,50.534380406110806],[23.280029296875,50.736455137010665],[23.48876953125,51.138001488062564],[22.785644531249996,51.984880139916626],[21.456298828125,52.6030475337285],[20.58837890625,52.90227586168308],[19.40185546875,53.26521293124656],[18.7646484375,53.51418452077113]]}'));
+            -- trip so that route start is within deviation of driver route
+            trips_within_deviation          AS (
+                SELECT trip_id,
+                       trip_route,
+                       trip_route_geom,
+                       trip_route_start
+                FROM trips_routes
+                WHERE ST_DWithin(driver_route, trip_route_start, driver_max_deviation_meters)
+            ),
 
-WITH
-    query_routes                           AS (
-        SELECT st_geomfromgeojson('{"type":"LineString","coordinates":[[23.48876953125,51.13110763758015],[22.049560546875,52.328625488430184],[20.302734375,53.00817326643286],[19.40185546875,53.25206880589411],[19.40185546875,54.15600109028491]]}') AS route
-    ),
-    closest_point_fractions                AS (
-        SELECT trip_route.name                                                         AS trip_name,
-               trip_route.route                                                        AS trip_route,
-               trip_route.max_deviation_meters                                         AS trip_max_deviation_meters,
-               query_routes.route                                                      AS query_route,
-               ST_LineLocatePoint(trip_route.route, st_startpoint(query_routes.route)) AS closest_point_fraction
-        FROM query_routes
-             LEFT JOIN trip_route
-             ON TRUE
-    ),
-    trip_effective_routes                  AS (
-        SELECT trip_name,
-               ST_LineSubstring(trip_route, closest_point_fraction, 1) AS trip_route,
-               trip_max_deviation_meters,
-               query_route
-        FROM closest_point_fractions
-        WHERE st_distance(
-                      st_startpoint(query_route)::geography,
-                      ST_LineInterpolatePoint(trip_route, closest_point_fraction)::geography
-                  ) < trip_max_deviation_meters
-    ),
-    trip_expanded_routes                   AS (
-        SELECT trip_name,
-               trip_route,
-               st_buffer(
-                       trip_route::geography,
-                       trip_max_deviation_meters,
-                       'endcap=flat join=round'
-                   )::geometry AS trip_route_polygon,
-               query_route
-        FROM trip_effective_routes
-    ),
-    common_routes                          AS (
-        SELECT trip_name,
-               query_route,
-               trip_route_polygon,
-               st_intersection(trip_route_polygon, query_route) AS common_route
-        FROM trip_expanded_routes
-    ),
-    common_routes_furthest_points          AS (
-        SELECT trip_name,
-               query_route,
-               trip_route_polygon,
-               common_route,
-               st_endpoint(
-                       st_geometryn(
-                               common_route,
-                               GREATEST(st_numgeometries(common_route) - 1, 1))
-                   ) AS furthest_point
-        FROM common_routes
-    ),
-    common_routes_furthest_point_fractions AS (
-        SELECT trip_name,
-               ST_LineLocatePoint(query_route, furthest_point) AS furthest_point_fraction
-        FROM common_routes_furthest_points
-    )
+            driver_after_pickup_routes      AS (
+                SELECT trip_id,
+                       trip_route,
+                       trip_route_geom,
+                       ST_LineSubstring(
+                               driver_route_geom,
+                               ST_LineLocatePoint(driver_route_geom, trip_route_start),
+                               1
+                           ) AS driver_after_pickup_route
+                FROM trips_within_deviation
+            ),
 
+            driver_after_pickup_polygons    AS (
+                SELECT trip_id,
+                       trip_route,
+                       trip_route_geom,
+                       st_buffer(
+                               driver_after_pickup_route::geography,
+                               driver_max_deviation_meters,
+                               'endcap=flat join=round'
+                           )::geometry AS driver_after_pickup_polygon
+                FROM driver_after_pickup_routes
+            ),
 
-SELECT *
-FROM common_routes_furthest_point_fractions
-ORDER BY furthest_point_fraction DESC
+            common_routes                   AS (
+                SELECT trip_id,
+                       trip_route,
+                       trip_route_geom,
+                       st_intersection(driver_after_pickup_polygon, trip_route) AS common_route
+                FROM driver_after_pickup_polygons
+            ),
+
+            common_routes_dropoff_points    AS (
+                SELECT trip_id,
+                       trip_route,
+                       trip_route_geom,
+                       st_endpoint(
+                               st_geometryn(
+                                       common_route::geometry,
+                                       GREATEST(st_numgeometries(common_route::geometry), 1))
+                           ) AS dropoff_point
+                FROM common_routes
+            ),
+            common_routes_dropoff_fractions AS (
+                SELECT trip_id,
+                       ST_LineLocatePoint(trip_route_geom, dropoff_point) AS dropoff_fraction
+                FROM common_routes_dropoff_points
+            )
+
+        SELECT trip_id
+        FROM common_routes_dropoff_fractions
+        WHERE dropoff_fraction IS NOT NULL
+        ORDER BY dropoff_fraction DESC
+        LIMIT 5;
+
+END;
+$$ LANGUAGE plpgsql STABLE;
