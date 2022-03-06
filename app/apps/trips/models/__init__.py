30608e6fef57f5452c4c982ae74a372612d74e91
Alex Rozum <rozumalex@gmail.com>
2022-03-06 11:43:56 +0100

    feat: add trip request report admin

--- app/apps/trips/models/__init__.py
+++ app/apps/trips/models/__init__.py
@@ -1,5 +1,5 @@
 from apps.trips.models.trip_proposals import *
-from apps.trips.models.trip_request_report import *
+from apps.trips.models.trip_request_reports import *
 from apps.trips.models.trip_requests import *
 from apps.trips.models.trips import *
 from apps.trips.models.waypoints import *
